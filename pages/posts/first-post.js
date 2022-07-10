e78dbd38bd188d182784e326223091dc829c6c7f
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-07-11 08:10:04 +0900

    :bento: 画像の読み込み、プロパティの動作確認

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Script from 'next/script';
+// import nextjs from '../../public/images/next-js.svg';
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>NestJS First Post</title>
-        <Script src="strategy='lazyOnlod' onLoad={()}" />
+        <Script src="strategy='lazyOnload' onLoad={()}" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
       </Head>
       <h1>First Blog Post</h1>
+      {/* <Image src={nextjs} /> */}
+      <Image
+        src="/images/next-js.svg"
+        width={300}
+        height={300}
+        // layout="fill"
+      />
     </>
   );
 }
